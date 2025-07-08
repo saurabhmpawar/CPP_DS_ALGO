544cb3a40e87f0d15c616d68c953c47cd280b5d5
Saurabh Pawar <saurabhpawar1396@gmail.com>
2025-07-08 20:25:08 +0530

    Create InputOutput.cpp

---
+++ Basic/InputOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace st
+
+int main() {
+    int age;
+    cout << "enter your age" <<endl ;
+    cin>> age;
+    cout << " your age is " << age << endl ;
+    
+    return 0;
+}
