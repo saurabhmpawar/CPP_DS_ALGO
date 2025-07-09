0fc5c6fc646982fcb92d5d18571faa1b652a1954
Saurabh Pawar <saurabhpawar1396@gmail.com>
2025-07-09 19:05:48 +0530

    Update basic.cpp

--- Straings/basic.cpp
+++ Straings/basic.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    
+    string str1 = "gg";
+    string str2 = "Pawar ";
+    string str3;
+    string str4;
+    
+    cout<<"Enter string ";
+    getline(cin,str4);
+    cout<<" \nstring 4 is "<<str4;
+    
+    str1 = "Saurabh ";  //dynamic
+    cout<<endl<<str1+str2;     //concatinate
+    str2 = "Saurabh ";
+    cout<<(str1==str2);   //compare
+    cout<<(str1<str2);   //lexiography comparision
+    cout<<str1.length();    //legnth
+    
+    cout<<"Enter string \n";
+    cin>>str3; //take input 
+    cout<<str3;   // till space only
+ 
+    //Loops on String -- for/foreach
+    
+    
+    
+    
+    return 0;
+}
