d0ab5b5e9b95cf03fa7289515b64ec7f9bbafe32
Saurabh Pawar <saurabhpawar1396@gmail.com>
2025-07-09 19:18:50 +0530

    Create reverse.cpp

---
+++ Straings/reverse.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include<string>
+#include <algorithm> 
+
+using namespace std;
+
+int main() {
+    string str = "apna college";
+    
+    reverse(str.begin(), str.end());
+    cout<<str<<endl;
+    
+    
+    return 0;
+}
