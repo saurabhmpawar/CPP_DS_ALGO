70830d2fd692007342b67daa9d6d18da162532f5
Saurabh Pawar <saurabhpawar1396@gmail.com>
2025-07-10 12:21:22 +0530

    Update Binary_search.cpp

--- searching/Binary_search.cpp
+++ searching/Binary_search.cpp
@@ -2,6 +2,30 @@
 #include <iostream>
 using namespace std;
 
+
+int Binary_search_recc(vector<int> arr, int target, int st, int end){
+
+     if(st<=end){
+        int mid = st + (end-st)/2;
+        
+        if(target > arr[mid]){
+           return Binary_search_recc(arr, target, mid+1, end);
+        }
+            
+        
+        else if(arr[mid] > target){
+           return Binary_search_recc(arr,target,st, mid-1);
+        }
+           
+        
+        else{
+            return mid;
+         }
+     }
+    return -1;
+}
+
+
 int Binary_search(vector <int> arr, int target){
      int st=0;
      int end=arr.size()-1;
@@ -29,7 +53,9 @@ int main() {
     
     int target = 13;
     
-   cout<<Binary_search(arr, target);
+   cout<<Binary_search(arr, target)<<endl;
+   cout<<Binary_search_recc(arr, target, 0, arr.size()-1);
+   
    
     return 0;
 }
