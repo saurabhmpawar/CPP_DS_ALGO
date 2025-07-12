189ef4bdfa000ff0f4ee30f6da3b18ab0518dc6e
Saurabh Pawar <saurabhpawar1396@gmail.com>
2025-07-12 13:32:54 +0530

    Create bubblesort.cpp

---
+++ sorting/bubble.cpp
@@ -0,0 +1,27 @@
+//bubble sort
+
+#include <iostream>
+#include<utility>
+#include<vector>
+using namespace std;
+
+int main() {
+    
+    vector<int> mat = {11,2,31,4,15,6};
+    
+   
+    cout<<"*********Bubble sort ;*******"<<endl;
+    
+    for(int i = 0; i<mat.size(); i++){
+        for(int j = i+1; j <mat.size(); j++){
+            if(mat[i]>mat[j])
+                swap(mat[i],mat[j]);
+        }
+    }
+    
+    for(int val: mat){
+        cout<< val<<" ";
+    }
+    
+    return 0;
+}
